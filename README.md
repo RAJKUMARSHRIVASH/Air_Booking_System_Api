f588c90ad09856e90ce1cfdce1e48efc556abd35
RAJ KUMAR SEN <rajkumarshrivash374@gmail.com>
2023-05-30 13:40:46 +0530

    all the CRUD operation has been performed successfully

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 # Air_Booking_System_Api
 
-## The API end points of the app are:-
+## The API end points of this app are:-
 
-1. api/user/register --> for registering of the user into the application
-2. api/user/login --> for login of the user into the application
-3. 
+1. **POST** `api/user/register` --> for registering of the user into the application
+2. **POST** `api/user/login` --> for login of the user into the application
+3. **POST** `api/flights` --> This endpoint will allow users to add new flights to the system.
+4. **GET** `api/flights` --> This endpoint will allow users to get all flights available.
+5. **GET** `api/flights/:id` --> This endpoint will allow users to get the specific flight of that id.
+6. **PATCH** `api/flights/:id` --> This patch request will allow users to update the specific flight of that id.
+7. **DELETE** `api/flights/:id` --> This DELETE request will allow users to DELETE the specific flight of that id.
+8. **POST** `api/booking` --> This endpoint will allow the user to book flights.
+9. **GET** `api/dashboard` --> This point will list all the bookings so far with the user and flight details.
--- routes/flightRoute.js
+++ routes/flightRoute.js
@@ -3,4 +3,71 @@ const FlightModel = require("../model/FlightModel");
 
 const flightRouter = express.Router();
 
+flightRouter.post("/", async (req, res) => {
+    let payload = req.body;
+    payload.departureTime = new Date().toISOString();
+    payload.arrivalTime = new Date().toISOString();
+    try {
+        const flight = new FlightModel(payload);
+        await flight.save();
+        res.json("New flight added")
+
+    } catch (error) {
+        res.json("Something went wrong " + error);
+    }
+});
+
+flightRouter.get("/", async (req, res) => {
+   
+    try {
+        const flight = await FlightModel.find();
+
+        res.json(flight)
+
+    } catch (error) {
+        res.json("Something went wrong " + error);
+    }
+});
+
+flightRouter.get("/:id", async (req, res) => {
+   const id = req.params.id;
+
+    try {
+        const flight = await FlightModel.findOne({_id:id});
+
+        res.json(flight)
+
+    } catch (error) {
+        res.json("Something went wrong " + error);
+    }
+});
+
+flightRouter.patch("/:id", async (req, res) => {
+   const id = req.params.id;
+   const payload = req.body;
+
+    try {
+        const flight = await FlightModel.findByIdAndUpdate({_id:id},payload);
+        res.json("Flight details updated")
+
+    } catch (error) {
+        res.json("Something went wrong " + error);
+    }
+});
+
+flightRouter.delete("/:id", async (req, res) => {
+   const id = req.params.id;
+
+    try {
+        const flight = await FlightModel.findByIdAndDelete({_id:id});
+        res.json("Flight has been deleted")
+
+    } catch (error) {
+        res.json("Something went wrong " + error);
+    }
+});
+
+
+
+
 module.exports = flightRouter;
