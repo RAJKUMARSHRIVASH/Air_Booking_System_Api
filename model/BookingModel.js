9cacda11287945595ee4353a79b846cdc5a7a878
Raj Kumar Sen <rajkumarshrivash374@gmail.com>
2023-06-01 11:03:49 +0530

    Update BookingModel.js
    imported the ObjectId from the types from mongoose.

--- model/BookingModel.js
+++ model/BookingModel.js
@@ -1,9 +1,12 @@
 const mongoose = require("mongoose");
+const { Schema, model, Types } = require("mongoose");
+const { ObjectId } = Types;
 const UserModel = require("./UserModel");
+const FlightModel = require("./FlightModel");
 
 const bookingSchema = mongoose.Schema({
-    user: { type: "ObjectId", ref: 'user' },
-    flight: { type: "ObjectId", ref: 'flight' }
+    user: { type: ObjectId, ref: 'UserModel' },
+    flight: { type: ObjectId, ref: 'FlightModel' }
 })
 
 const BookingModel = mongoose.model("booking", bookingSchema);
